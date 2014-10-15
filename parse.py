de24e79d265bfd693f1eab5dbed94ffef3f9d8ec
LHMike <lhmike1119@gmail.com>
2014-10-15 22:26:30 +0800

    调试解析评论

---
+++ parse.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import re
+import string
+
+def parseNotice(noticeText):
+    #rule = re.compile(ur'\d+&&comment\'\)">(\w+|[\u4e00-\u9fa5]+)')
+    rule = re.compile(ur'\d+&&comment\'\)">.+')
+    array = rule.findall(unicode(noticeText,'utf-8')) 
+    for val in array:
+        print val
+
+if __name__ == "__main__":
+    f = open('./test','r')
+    ncText = f.read()
+    f.close()
+    parseNotice(ncText)
